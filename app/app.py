3089328070ac95149bb961d58194ada79f43efee
yukessh <yukeshempty@gmail.com>
2025-12-27 23:50:13 +0530

    day3

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect,url_for
 from models import db,User
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 app=Flask(__name__)
 app.secret_key="dev-secret"
@@ -23,7 +25,8 @@ def register():
         password=request.form['password']
         email=request.form['email']
         
-        user=User(username=username,password=password,email=email,role="user")
+        hashed_password=generate_password_hash(password)
+        user=User(username=username,password=hashed_password,email=email,role="user")
         
         db.session.add(user)
         db.session.commit()
@@ -32,6 +35,32 @@ def register():
         
     
     return render_template("register.html")
+
+@app.route("/login",methods=['GET','POST'])
+
+def login():
+
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+
+        user=User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password,password):
+            session["user_id"]=user.id
+            return redirect(url_for("dashboard"))
+        
+        else:
+            return "Invalid credentials" 
+    
+    return render_template("login.html")
+
+@app.route("/dashboard")
+
+def dashboard():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+    return "Welcome to your dashboard"
 
 with app.app_context():
     db.create_all()
