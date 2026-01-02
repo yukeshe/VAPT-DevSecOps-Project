2da93a032b628f50e5982b54558d6996a379efe0
yukessh <yukeshempty@gmail.com>
2026-01-03 00:01:07 +0530

    siem integration

--- app/models.py
+++ app/models.py
@@ -11,8 +11,8 @@ class User(db.Model):
     role=db.Column(db.String(20))
 
 class Securitylog(db.Model):
-    id=db.column(db.Integer, primary_key=True)
-    event_type=db.column(db.String(100))
-    user_id=db.column(db.Integer)
-    ip_address=db.column(db.String(50))
-    timestamp=db.column(db.DateTime, default=datetime)
+    id=db.Column(db.Integer, primary_key=True)
+    event_type=db.Column(db.String(100))
+    user_id=db.Column(db.Integer)
+    ip_address=db.Column(db.String(50))
+    timestamp=db.Column(db.DateTime, default=datetime)
